8bd50f714fd892ae1542813dee3574dec7fe0d24
cha196 <cha196@sfu.ca>
2023-11-29 11:03:12 -0800

    complete fetching weather data

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -1,16 +1,103 @@
 import requests, yaml, json
+import aiohttp, asyncio
+from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 
 with open('config/config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
+locations = [{"q": city+', British Columnbia'} for city in config['CITIES']]
+payloads = [{"locations": locations[:int(len(config['CITIES'])/2)]}, 
+            {"locations": locations[int(len(config['CITIES'])/2):]}]
 
-def bulk_weather_request(data_type='current'):
+
+def get_last_days(start_date_str=datetime.now().strftime('%Y-%m-%d'), num_days=config['HISTORY_DAYS']):
+
+    date_format = "%Y-%m-%d"
+    last_dates = []
+
+    start_date = datetime.strptime(start_date_str, date_format)
+
+    for i in range(num_days):
+        last_date = start_date - timedelta(days=i+1)
+        last_dates.append(last_date.strftime(date_format))
+
+    return last_dates
+
+
+def extract_current(input_dict):
+
+    return {
+        'name': input_dict['query']['location']['name'],
+        'lat': input_dict['query']['location']['lat'],
+        'lon': input_dict['query']['location']['lon'],
+        'last_updated': input_dict['query']['current']['last_updated'],
+        'temp_c': input_dict['query']['current']['temp_c'],
+        'wind_degree': input_dict['query']['current']['wind_degree'],
+        'wind_dir': input_dict['query']['current']['wind_dir'],
+        'cloud': input_dict['query']['current']['cloud'],
+        'humidity': input_dict['query']['current']['humidity'],
+        'pm2_5': input_dict['query']['current']['air_quality']['pm2_5'],
+        'condition': input_dict['query']['current']['condition']['text'],
+        'condition_icon_link': 'https://'+input_dict['query']['current']['condition']['icon']
+}
+
+
+def extract_history(input_dict):
+
+    return {
+        'name': input_dict['query']['location']['name'],
+        'lat': input_dict['query']['location']['lat'],
+        'lon': input_dict['query']['location']['lon'],
+        'date': input_dict['query']['forecast']['forecastday'][0]['date'],
+        'maxtemp_c': input_dict['query']['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        'mintemp_c': input_dict['query']['forecast']['forecastday'][0]['day']['mintemp_c'],
+        'avgtemp_c': input_dict['query']['forecast']['forecastday'][0]['day']['avgtemp_c'],
+        'avghumidity': input_dict['query']['forecast']['forecastday'][0]['day']['avghumidity'],
+        'condition': input_dict['query']['forecast']['forecastday'][0]['day']['condition']['text'],
+        'condition_icon_link': 'https:'+input_dict['query']['forecast']['forecastday'][0]['day']['condition']['icon']
+    }
+    
+
+
+def extract_forecast(input_dict):
+    """
+    load next 3 days data
+    """
+    results = []
+
+    for day in range(1, config['FORECAST_DAYS']+1):
+        result = {
+            'name': input_dict['query']['location']['name'],
+            'lat': input_dict['query']['location']['lat'],
+            'lon': input_dict['query']['location']['lon'],
+            'date': input_dict['query']['forecast']['forecastday'][day]['date'],
+            'maxtemp_c': input_dict['query']['forecast']['forecastday'][day]['day']['maxtemp_c'],
+            'mintemp_c': input_dict['query']['forecast']['forecastday'][day]['day']['mintemp_c'],
+            'avgtemp_c': input_dict['query']['forecast']['forecastday'][day]['day']['avgtemp_c'],
+            'avghumidity': input_dict['query']['forecast']['forecastday'][day]['day']['avghumidity'],
+            'daily_chance_of_rain': input_dict['query']['forecast']['forecastday'][day]['day']['daily_chance_of_rain'],
+            'daily_chance_of_snow': input_dict['query']['forecast']['forecastday'][day]['day']['daily_chance_of_snow'],
+            'pm2_5': input_dict['query']['forecast']['forecastday'][day]['day']['air_quality']['pm2_5'],
+            'condition': input_dict['query']['forecast']['forecastday'][day]['day']['condition']['text'],
+            'condition_icon_link': 'https:'+input_dict['query']['forecast']['forecastday'][day]['day']['condition']['icon']
+        }
+        results.append(result)
+
+    return results
+
+
+def fetch_current(aqi=True):
+
+    """
+    Fetch current weather data for all 52 cities in British Columnbia.
+    """
 
     raw_data = []
 
     def fetch_weather(payload):
-        url = f"http://api.weatherapi.com/v1/{data_type}.json?key={config['API_KEY'][0]}&q=bulk"
+        url = f"http://api.weatherapi.com/v1/current.json?key={config['API_KEY'][0]}&q=bulk{'&aqi=yes' if aqi == True else ''}"
         headers = {'Content-Type': 'application/json'}
         response = requests.post(url, headers=headers, data=json.dumps(payload))
         if response.status_code == 200:
@@ -20,17 +107,71 @@ def bulk_weather_request(data_type='current'):
             print(response.text)
             return None
 
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(fetch_weather, payloads)
 
-    locations = [{"q": city+', British Columnbia'} for city in config['CITIES']]
-    payloads = [{
-        "locations": locations[:int(len(config['CITIES'])/2)]
-    }, 
-    {
-        "locations": locations[int(len(config['CITIES'])/2):]
-    }]
+    full_raw_data = raw_data[0]['bulk'] + raw_data[1]['bulk']
+
+    return [extract_current(d) for d in full_raw_data]
+
+
+def fetch_forecast(days=config['FORECAST_DAYS']+1, aqi=True):
+
+    """
+    Fetch next 3 days forecast weather data for all 52 cities in British Columnbia.
+    Granularity: by date
+    """
+
+    raw_data = []
+
+    def fetch_weather(payload):
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={config['API_KEY']}&q=bulk{'&aqi=yes' if aqi == True else ''}&days={days}"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        if response.status_code == 200:
+            raw_data.append(response.json())
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            print(response.text)
+            return None
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         executor.map(fetch_weather, payloads)
 
-    return raw_data[0]['bulk'] + raw_data[1]['bulk']
+    full_raw_data = raw_data[0]['bulk'] + raw_data[1]['bulk']
+
+    return [j for i in [extract_forecast(d) for d in full_raw_data] for j in i]
+
+
+def fetch_history(aqi=True):
+
+    """
+    Fetch last 7 days historical weather data for all 52 cities in British Columnbia.
+    Granularity: by date
+    """
+
+    full_raw_data_list = []
+    raw_data = []
+    history_days = get_last_days()
+
+    def fetch_weather(date_str, payload):
+        url = f"http://api.weatherapi.com/v1/history.json?key={config['API_KEY']}&q=bulk{'&aqi=yes' if aqi == True else ''}&dt={date_str}"
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        if response.status_code == 200:
+            raw_data.append(response.json())
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+            print(response.text)
+            return None
+
+    for day in history_days:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+                partial_fetch_weather = partial(fetch_weather, day)
+                executor.map(partial_fetch_weather, payloads)
+        full_raw_data_list.append(raw_data[0]['bulk'] + raw_data[1]['bulk'])
+        raw_data = []
+
+    return [extract_history(d) for d in [j for i in full_raw_data_list for j in i]]
+
     
