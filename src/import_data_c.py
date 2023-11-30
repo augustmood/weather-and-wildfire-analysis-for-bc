82ae2d8aaacd4705a801b7283ecd798e6146d091
cha196 <cha196@sfu.ca>
2023-11-30 14:37:44 -0800

    import data checkpoint

---
+++ src/import_data_c.py
@@ -0,0 +1,47 @@
+from cassandra.cluster import Cluster
+from datetime import datetime
+import os, sys, re, yaml
+from cassandra.query import BatchStatement, SimpleStatement
+from data_fetch import fetch_current
+
+
+def main(config):
+    cluster = Cluster(['node1.local', 'node2.local'])
+    session = cluster.connect()
+    session.execute("""
+    CREATE TABLE IF NOT EXISTS current_weather (city TEXT,
+        lat DECIMAL,
+        lon DECIMAL,
+        last_updated TIMESTAMP,
+        temp_c DECIMAL,
+        wind_kph DECIMAL,
+        wind_degree INT,
+        wind_dir TEXT,
+        cloud INT,
+        humidity INT,
+        pm2_5 FLOAT,
+        condition TEXT,
+        condition_icon_link TEXT,
+        PRIMARY KEY (city)
+    )""")
+
+    data_current = fetch_current()
+
+    batch_statement = "BEGIN BATCH "
+    for row in data_current:
+        batch_statement += f"""
+        INSERT INTO {config['KEYSPACE']}.current_weather 
+        {str(tuple(config['CURRENT_COLUMNS'])).replace("'","")} VALUES {tuple(row)};"""
+    batch_statement += "\nAPPLY BATCH;"
+    session.execute(batch_statement)
+
+    session.shutdown()
+
+
+
+if __name__=="__main__":
+
+    with open('../config/config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    main(config)
