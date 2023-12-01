dcf4cf422ac5ae8ba6b3bc183000dced4ab7215a
Binming Li <bla175@sfu.ca>
2023-11-30 16:04:56 -0800

    Update scripts for loading wildfire data: - Now 'spark_wild_fire_locations.py' can read wildfire data and produce two tables, join the tables, and load them into the Cassandra. - Update requirements.txt - Update 'wildfire_data_scraping': download datasets from given url by 'requests'

--- wildfire_data_scraping.py
+++ wildfire_data_scraping.py
@@ -1,47 +1,20 @@
-from selenium import webdriver
 import requests
-import shutil
-import time
 import os
 
-def download_file(url, destination):
-    # Start Chrome WebDriver
-    driver = webdriver.Chrome()
-
-    try:
-        # Open the URL
-        driver.get(url)
-
-        # Wait for some time to allow the download to start (adjust as needed)
-        time.sleep(5)
-
-        # Get the download link
-        download_link = driver.find_element_by_xpath('//a[contains(@href,"download")]')
-
-        # Get the download URL
-        download_url = download_link.get_attribute("href")
-
-        # Use requests to download the file
-        with requests.get(download_url, stream=True) as response:
-            with open(destination, 'wb') as file:
-                shutil.copyfileobj(response.raw, file)
-
-        print(f"File downloaded to: {destination}")
-
-    finally:
-        # Close the WebDriver
-        driver.quit()
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as response:
+        if response.status_code == 200:
+            with open(local_filename, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f"Downloaded: {local_filename}")
+        else:
+            print(f"Failed to download: {url}")
+            print(f"Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    # TO DO: add our file path!!!
-    if os.path.exists("/Users/dexinyang/Downloads/prot_current_fire_polys.zip"):
-        os.remove("/Users/dexinyang/Downloads/prot_current_fire_polys.zip")
-        file_url = "https://pub.data.gov.bc.ca/datasets/cdfc2d7b-c046-4bf0-90ac-4897232619e1/prot_current_fire_polys.zip"
-        destination_path = "/Users/dexinyang/Downloads/prot_current_fire_polys.zip"
-
-        download_file(file_url, destination_path)
-    else:
-        file_url = "https://pub.data.gov.bc.ca/datasets/cdfc2d7b-c046-4bf0-90ac-4897232619e1/prot_current_fire_polys.zip"
-        destination_path = "/Users/dexinyang/Downloads/prot_current_fire_polys.zip"
-
-        download_file(file_url, destination_path)
+    file_url = "https://pub.data.gov.bc.ca/datasets/cdfc2d7b-c046-4bf0-90ac-4897232619e1/prot_current_fire_polys.zip"
+    destination_path = "./prot_current_fire_polys.zip"
+    if os.path.exists("./prot_current_fire_polys.zip"):
+        os.remove("./prot_current_fire_polys.zip")
+    download_file(file_url, destination_path)
