c02fdcfa227311ee9759afba9a3cbe258e88f1ce
Dexin Yang <dya63@sfu.ca>
2023-12-02 16:27:36 -0800

    MAJOR - the basic structure of a web app

---
+++ webapp/plotter.py
@@ -0,0 +1,9 @@
+from typing import Union,List,Dict
+import datetime as dt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+from operator import concat
+import math
+
