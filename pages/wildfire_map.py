5f2dd97b562a0c8a33e1c1167095d1d46e1e8b96
Binming Li <bla175@sfu.ca>
2023-12-08 05:11:03 -0800

    Update wildfire_map page.

--- pages/wildfire_map.py
+++ pages/wildfire_map.py
@@ -25,16 +25,5 @@ map_fig = px.scatter_mapbox(map_data, lat="latitude", lon="longitude", color='fi
                   custom_data=['fire_link'])
 
 wildfire_map = dcc.Graph(id='scatter-map', figure=map_fig,  style={'width': '90%', 'height': '700px'})
-# Callback to open the URL in a new tab when a scatter marker is clicked
-@callback(
-    Output('scatter-map', 'selectedData'),
-    [Input('scatter-map', 'clickData')]
-)
-def open_url_in_new_tab(click_data):
-    if click_data:
-        selected_url = click_data['points'][0].get('customdata', None)[0]
-        if selected_url:
-            webbrowser.open_new_tab(selected_url)
-    return None
 
 layout = html.Div(children=[wildfire_map])
